bb347489b61321df95c0523071a34b81e1f03234
Grigory <grigory@clearscale.net>
2021-11-14 19:17:40 +0700

    Routing added (frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import {Navigate, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/home" element={<h1>Home</h1>} />
+                <Route path="/" element={<Navigate to="/home" />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
